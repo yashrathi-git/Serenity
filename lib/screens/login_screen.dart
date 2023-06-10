0f2de6dcc8c8341bcc2c2517a0b28af3f9c00879
Amritansh Sharma <amritansh.code@gmail.com>
2023-06-11 04:27:29 +0530

    added custom signin button

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -193,15 +193,63 @@ class LoginPage extends StatelessWidget {
                   }).toList(),
                 ),
                 const SizedBox(height: 40),
-                SignInButton(
-                  Buttons.Google,
+                CustomSignInButton(
                   onPressed: () async {
                     await _signInWithGoogle(context);
                   },
-                ),
+                )
               ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomSignInButton extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  CustomSignInButton({required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        height: 48.0,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(24.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/google.gif',
+              height: 30.0,
+              width: 30.0,
+            ),
+            SizedBox(width: 8.0),
+            Text(
+              'Sign in with Google',
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                backgroundColor: Colors.black,
+              ),
+            ),
+          ],
         ),
       ),
     );

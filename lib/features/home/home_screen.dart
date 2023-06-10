022521b6be8a9149fb61a20bfbaef3093aba7706
Amritansh Sharma <amritansh.code@gmail.com>
2023-06-10 20:59:14 +0530

    added gender prompt

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,6 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  final bool justLoggedIn;
+
+  HomePage({required this.justLoggedIn});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isFemale = false;
+  bool isPromptShown = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      _checkPromptStatus();
+    });
+  }
+
+  Future<void> _checkPromptStatus() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final bool? promptShown = prefs.getBool('promptShown');
+
+    if (promptShown == true) {
+      setState(() {
+        isPromptShown = true;
+      });
+    } else if (widget.justLoggedIn) {
+      _showPrompt();
+    }
+  }
+
+  Future<void> _showPrompt() async {
+    final User? user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      return;
+    }
+
+    final gender = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Gender'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                title: Text('Male'),
+                onTap: () {
+                  Navigator.pop(context, false);
+                },
+              ),
+              ListTile(
+                title: Text('Female'),
+                onTap: () {
+                  Navigator.pop(context, true);
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+
+    if (gender != null) {
+      setState(() {
+        isFemale = gender;
+      });
+
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .set({'isFemale': isFemale});
+    }
+
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('promptShown', true);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/features/home/login.dart
+++ lib/features/home/login.dart
@@ -4,12 +4,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:serenity/features/home/home_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPage extends StatelessWidget {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final String loggedInKey = 'loggedIn';
 
-  Future<UserCredential> _signInWithGoogle() async {
+  Future<UserCredential> _signInWithGoogle(BuildContext context) async {
     final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
     final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
 
@@ -18,46 +20,76 @@ class LoginPage extends StatelessWidget {
       idToken: googleAuth.idToken,
     );
 
-    final UserCredential userCredential = await _auth.signInWithCredential(credential);
-    final User? user = userCredential.user;
+    try {
+      final UserCredential userCredential = await _auth.signInWithCredential(credential);
 
-    return userCredential;
+      // Store login status
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setBool(loggedInKey, true);
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage(justLoggedIn: true,)),
+      );
+
+      return userCredential;
+    } catch (e) {
+      print(e.toString());
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Error'),
+          content: Text('Failed to sign in with Google.'),
+        ),
+      );
+      throw e;
+    }
+  }
+
+  Future<void> _checkLoggedIn(BuildContext context) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final bool? loggedIn = prefs.getBool(loggedInKey);
+
+    if (loggedIn == true) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage(justLoggedIn: true,)),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    _checkLoggedIn(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login with Google'),
+        title: Text(
+          'Serenity',
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SignInButton(
-              Buttons.Google,
-              onPressed: () async {
-                try {
-                  await _signInWithGoogle();
-                  // Navigate to the home page after successful login
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomePage()),
-                  );
-                } catch (e) {
-                  print(e.toString());
-                  // Show an error message if login fails
-                  showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                      title: Text('Error'),
-                      content: Text('Failed to sign in with Google.'),
-                    ),
-                  );
-                }
-              },
+      body: Container(
+        color: Colors.grey[900],
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SignInButton(
+                  Buttons.Google,
+                  onPressed: () async {
+                    await _signInWithGoogle(context);
+                  },
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
